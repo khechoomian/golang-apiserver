a43c21405c27344004b45675f62cdf5496d68e89
Frik Khechoomian <frik.khecho@gmail.com>
2021-12-03 22:30:26 +0330

    add godotenv

--- cmd/golang-apiserver/apiserver.go
+++ cmd/golang-apiserver/apiserver.go
@@ -5,6 +5,19 @@ Copyright 2021
 // apiserver is the main api server
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/joho/godotenv"
+)
+
+func main() {
+}
+
+func init() {
+	//TODO this section must be change into another file and bether construct
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module golang-apiserver
 
 go 1.16
+
+require github.com/joho/godotenv v1.4.0
